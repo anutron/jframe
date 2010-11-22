6bafb659d773298fc6ef1e650313f5e82144a922
Aaron Newton <git@clientcide.com>
2010-11-22 11:59:41 -0800

    moving flash message stuff around

--- Source/UI/FlashMessage.js
+++ Source/UI/FlashMessage.js
@@ -15,7 +15,7 @@
 // limitations under the License.
 /*
 ---
-description: Flash Messaging (notifications) for Desktop
+description: Flash Messaging (notifications) for JFrame
 provides: [FlashMessage]
 requires: [Core/Fx.Tween, Core/Fx.Transitions]
 script: FlashMessage.js
@@ -31,17 +31,28 @@ script: FlashMessage.js
 	returns: a function that will hide the message when called. There is no consequence to calling it if the message
 	  has already been cleaned (or calling it more than once).
 */
-FlashMessage = {
-	growl: function(message, duration, noCleanup){
-		var b = 100; // offset for get satifaction 
+var FlashMessage = {
+	messages: [],
+	flash: function(options){
+		options = $merge({
+			message: null,
+			noCleanup: false,
+			duration: 4500,
+			minimumTime: 2000,
+			target: document.body,
+			offset: 10,
+			margin: 12
+		}, options);
+		var container = new Element('div', { 'class': 'growl_container' }).inject(options.target);
+		var b = options.offset; // offset for get satifaction 
 	
 		var growls = $$('.growl');
 		if(growls.length){ // if growls already exist
 			var bottom = growls[growls.length-1].getStyle('bottom').toInt();
-			b = bottom + growls[growls.length-1].getSize().y + 12;
+			b = bottom + growls[growls.length-1].getSize().y + options.margin;
 		}
 		//store the message as displayed so we don't show dupes
-		this.messages.push(message);
+		this.messages.push(options.message);
 		var el, cleaned, cleanupCallbackCalledAlready, okToClean, timeout;
 		//create a timeout for a minimum amount of time for a message to be displayed
 		//if the cleanup method is called before this timeout, wait until the timer expires
@@ -49,7 +60,7 @@ FlashMessage = {
 		(function(){ 
 			okToClean = true;
 			if (cleanupCallbackCalledAlready) cleanup();
-		}).delay(2000);
+		}).delay(options.minimumTime);
 		//this function removes the message
 		var cleanup = function(clicked){
 			//if the user clicked the message, or the timer has expired,
@@ -64,14 +75,14 @@ FlashMessage = {
 			$clear(timeout);
 			//slide the element out to the right, then destroy it and remove the message from the stack
 			el.tween('right', -300).get('tween').chain(function(){
-				this.messages.erase(message);
+				FlashMessage.messages.erase(options.message);
 				el.destroy();
-			}.bind(this));
+			});
 		};
 		//create the element and inject it into the DOM
 		el = new Element('div',{
 			'class': 'growl', 
-			html: message,
+			html: options.message,
 			tween: {
 				transition: 'back:in'
 			},
@@ -82,14 +93,13 @@ FlashMessage = {
 				right : 0,
 				bottom: b
 			}
-		}).inject(document.body);
+		}).inject(container);
 		//set a timeout to hide the message
-		if (!noCleanup) timeout = cleanup.delay(duration || 4500);
+		if (!options.noCleanup) timeout = cleanup.delay(options.duration);
 		//return the cleanup function so that the code that called this method can optionally
 		//clean it before the timeout.
 		return cleanup;
 	},
-	messages: [],
 	checkForFlashMessage: function(message) {
 		return this.messages.contains(message);
 	}
